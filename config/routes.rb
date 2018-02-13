b34205ce608a5389c0c3152e46d2a26bdebe35b0
Nathalie Roch <nathalie.m.roch@gmail.com>
2018-02-13 09:35:45 +0100

    column

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #read
+  #read all
+
+  get 'restaurants', to: 'restaurants#index'
+  #read one
+  get 'restaurants/:id', to: 'restaurants#show'
+
+  get 'restaurants/new', to: 'restaurants#new'
 end
